9bbb6baa2688b0f75e5d976abeee19aabeca16cb
libo <libo@usertech.cn>
2017-07-27 09:15:23 +0000

    modify startup.sh to change own of log files

--- startup.sh
+++ startup.sh
@@ -1,3 +1,8 @@
 #nohup python -u send.py > send.log 2>&1 &
 #nohup python -u receive.py > receive.log 2>&1 &
-nohup python -u all.py > all.log 2>&1 &
+>/home/pi/python/coffee.log
+>/home/pi/python/all.log
+/bin/chown pi:pi /home/pi/python/coffee.log
+/bin/chown pi:pi /home/pi/python/all.log
+nohup python -u /home/pi/python/all.py > /home/pi/python/all.log 2>&1 &
+#nohup python -u all.py > all.log 2>&1 &
