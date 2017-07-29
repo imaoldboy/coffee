7ecca164540f10d440df71f8f42915c9667b2c43
libo <1445745@qq.com>
2017-07-29 01:28:38 +0000

    change python to coffee

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 #nohup python -u receive.py > receive.log 2>&1 &
 >/home/pi/python/coffee.log
 >/home/pi/python/all.log
-/bin/chown pi:pi /home/pi/python/coffee.log
-/bin/chown pi:pi /home/pi/python/all.log
-nohup python -u /home/pi/python/all.py > /home/pi/python/all.log 2>&1 &
+/bin/chown pi:pi /home/pi/coffee/coffee.log
+/bin/chown pi:pi /home/pi/coffee/all.log
+nohup python -u /home/pi/coffee/all.py > /home/pi/coffee/all.log 2>&1 &
 #nohup python -u all.py > all.log 2>&1 &
